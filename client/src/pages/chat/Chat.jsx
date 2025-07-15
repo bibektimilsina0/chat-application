8967f856aa3ce276dfefe21ceeb12223739d013f
bikkibro <bibektimilsina7857@gmail.com>
2025-07-15 23:40:09 +0545

    backend url fixed for io

--- client/src/pages/chat/Chat.jsx
+++ client/src/pages/chat/Chat.jsx
@@ -91,7 +91,7 @@ function Chat() {
     useEffect(() => {
         dispatch(fetchFriends());
         
-        const newSocket = io('http://localhost:5000', {
+        const newSocket = io(import.meta.env.VITE_BACKEND_URL, {
             withCredentials: true
         });
         
